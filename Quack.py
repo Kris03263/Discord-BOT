cc5ec494c13648f8a5b070edaac65ea77e7e619c
Kris03263 <88489735+Kris03263@users.noreply.github.com>
2021-09-01 21:39:25 +0800

    Create Quack.py

---
+++ Quack.py
@@ -0,0 +1,26 @@
+import requests
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, cog
+from classes import Cog_Extension
+
+class Quack(Cog_Extension):
+    @commands.command()
+    async def Quack(self,ctx): 
+        url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization=CWB-5C60CAF7-5596-451E-B4DF-4B9CC51ABB59&downloadType=WEB&format=JSON'
+        # 要求網站回傳
+        res = requests.get(url)
+        # 將回傳值的 json 檔丟入 data_we
+        data_qu = res.json()
+        # 顯示數值
+        embed=discord.Embed(title="*地震報告*", url="https://www.cwb.gov.tw/V8/C/", description="來自呱呱特派員", color=0xb806f9)
+        embed.set_author(name="中央氣象局", url="https://www.cwb.gov.tw/V8/C/", icon_url="https://i.imgur.com/MOL5A8l.jpg")
+        embed.add_field(name="報告編號", value=data_qu['records']['earthquake'][0]['earthquakeNo'], inline=True)
+        embed.add_field(name="跌倒時間", value=data_qu['records']['earthquake'][0]['earthquakeInfo']['originTime'], inline=True)
+        embed.add_field(name="在哪跌倒", value=data_qu['records']['earthquake'][0]['earthquakeInfo']['epiCenter']['location'], inline=False)
+        embed.add_field(name="芮氏規模", value=data_qu['records']['earthquake'][0]['earthquakeInfo']['magnitude']['magnitudeValue'], inline=True)
+        embed.add_field(name="深度(公里)", value=data_qu['records']['earthquake'][0]['earthquakeInfo']['depth']["value"], inline=True)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Quack(bot))
