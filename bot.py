bdb1ef37a4c8d12c71e65e977ae290e726a3ad05
Kris03263 <88489735+Kris03263@users.noreply.github.com>
2021-09-01 21:29:20 +0800

    Update and rename README.md to bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import random
+import json
+import requests
+import time
+import datetime
+import os
+from pprint import pprint
+from discord.ext import commands
+from discord.ext.commands import Bot
+bot = commands.Bot(command_prefix='#') # 機器人指令的觸發文字
+
+for Filename in os.listdir('DiscordBot/Functions'):
+    if Filename.endswith('.py'):
+        bot.load_extension(F'Functions.{Filename[:-3]}')
+
+if __name__ == "__main__":
+    bot.run('ODcyNzk4MDk3MzMwMjg2NjA0.YQvGkg.6j_jdWqaNDhV8p_ZL9YxkNfMjA8') # bot 的 token
+  
